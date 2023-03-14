1632f1cfea82eceb3c426dedc1dcfa78b747f6d5
colepfeiffer.sol <114104437+Yurei7@users.noreply.github.com>
2023-03-14 16:06:03 -0400

    Refactor code

--- dice.js
+++ dice.js
@@ -1,84 +1,37 @@
 
+const picUrls = [
+    "https://cdn-icons-png.flaticon.com/512/8336/8336943.png",
+    "https://cdn-icons-png.flaticon.com/512/8336/8336956.png",
+    "https://cdn-icons-png.flaticon.com/512/8336/8336955.png",
+    "https://cdn-icons-png.flaticon.com/512/8336/8336933.png",
+    "https://cdn-icons-png.flaticon.com/512/8336/8336931.png",
+    "https://cdn-icons-png.flaticon.com/512/8336/8336948.png"
+];
+
+const diceMax = 6;
+
 function diceGame() {
-    document.querySelector(".btn").style.visibility=("visible")
+    document.querySelector(".btn").style.visibility = ("visible")
 
+    let randomDice1 = Math.floor((Math.random() * diceMax) + 1)
+    let randomDice2 = Math.floor((Math.random() * diceMax) + 1)
 
-    let randomDice1= Math.floor((Math.random()*6)+1)
-    if (randomDice1===1){
-        document.querySelector(".player1").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336943.png")
-    
+    document.querySelector(".player1").setAttribute("src", picUrls[randomDice1 - 1]);
+    document.querySelector(".player2").setAttribute("src", picUrls[randomDice2 - 1]);
+
+    if (randomDice1 > randomDice2) {
+        document.querySelector(".title").innerHTML = ("&#129351; Player 1 Wins! &#129351;")
+
     }
-    
-    else if (randomDice1===2) {
-        document.querySelector(".player1").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336956.png")
-    
+    else if (randomDice2 > randomDice1) {
+        document.querySelector(".title").innerHTML = ("&#129351; Player 2 Wins! &#129351;")
+
     }
-    
-    else if (randomDice1===3) {
-        document.querySelector(".player1").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336955.png")
-    
+    else if (randomDice1 === randomDice2) {
+        document.querySelector(".title").innerHTML = (" &#129309; DRAW! &#129309;")
+
     }
-    else if (randomDice1===4) {
-        document.querySelector(".player1").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336933.png")
-        
-    
-    }
-    
-    else if (randomDice1===5) {
-        document.querySelector(".player1").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336931.png")
-    
-    }
-    
-    else if (randomDice1===6) {
-        document.querySelector(".player1").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336948.png")
-    
-    }
-    
-    let randomDice2= Math.floor((Math.random()*6)+1)
-    
-    
-    if (randomDice2===1){
-        document.querySelector(".player2").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336943.png")
-    
-    }
-    
-    else if (randomDice2===2) {
-        document.querySelector(".player2").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336956.png")
-    
-    }
-    
-    else if (randomDice2===3) {
-        document.querySelector(".player2").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336955.png")
-    
-    }
-    else if (randomDice2===4) {
-        document.querySelector(".player2").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336933.png")
-    
-    }
-    
-    else if (randomDice2===5) {
-        document.querySelector(".player2").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336931.png")
-    
-    }
-    
-    else if (randomDice2===6) {
-        document.querySelector(".player2").setAttribute("src", "https://cdn-icons-png.flaticon.com/512/8336/8336948.png")
-    
-    }
-    
-    if (randomDice1>randomDice2) {
-        document.querySelector(".title").innerHTML=("&#129351; Player 1 Wins! &#129351;")
-    
-    }
-    else if (randomDice2>randomDice1) {
-        document.querySelector(".title").innerHTML=("&#129351; Player 2 Wins! &#129351;")
-    
-    }
-    else if (randomDice1===randomDice2){
-     document.querySelector(".title").innerHTML=(" &#129309; DRAW! &#129309;")
-    
-    }
-    
+
 
 }
 
